484a2e9abbe6f705e8c3ed469be7d8fc6565f599
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-18 02:16:20 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -2,8 +2,6 @@ require 'yaml'
 require 'pp'
 
 def directors_database
-pp	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
-    @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-   
+pp	
    
 end
